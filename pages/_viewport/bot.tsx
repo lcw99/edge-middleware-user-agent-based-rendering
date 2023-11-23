2dea7886492b4d7408ca0b54423fac5e6f00620d
chang <changlee99@gmail.com>
2023-11-23 16:34:40 +0900

    head

--- pages/_viewport/bot.tsx
+++ pages/_viewport/bot.tsx
@@ -1,24 +1,24 @@
 import { useRouter } from 'next/router';
 import React from 'react';
-import { Helmet } from 'react-helmet';
+import Head from 'next/head'
 
 interface Props {}
 
 const Detail: React.FunctionComponent<Props> = () => {
   const router = useRouter();
   console.log(router.query)
-  return <div>
-    <Helmet>
+  return <>
+    <Head>
       <title>{`${false ? "Admin Title" : "test1"}`}</title>
       <meta name="description" content={`${false ? "Admin Content" : "test2"}`} />
       <meta property="og:title" content="새로운선택"/>
       <meta property="og:description" content="새로운선택"/>
       <meta property="og:image" content="https://newparty.kr/icons/newparty.jpg"/>
       <meta property="og:type" content="website"/>
-    </Helmet>
+    </Head>
     bot test
     <div>b: {router.query.md}</div>
-  </div>;
+  </>;
 };
 
 export default Detail;
