e192fc9bae11899acb6ebc2049a1b04cb7da3973
chang <changlee99@gmail.com>
2023-11-23 17:26:54 +0900

    meta tag

--- pages/_viewport/bot.tsx
+++ pages/_viewport/bot.tsx
@@ -2,27 +2,48 @@ import { useRouter } from 'next/router';
 import React from 'react';
 import Head from 'next/head'
 
-interface Props {}
+type Repo = {
+  title: string
+  author: string
+  pub_date: string
+  article: string
+  abstracts: string
+}
 
-const Detail: React.FunctionComponent<Props> = () => {
-  const router = useRouter();
-  console.log(router.query)
+const Detail: React.FunctionComponent<Repo> = (repo) => {
+  // const product = await fetch(`https://lcw99.github.io/newparty-cms/article/articles.json`).then((res) => res.json())
+  // console.log(repo)
   return <>
     <Head>
-      <title>{`${false ? "Admin Title" : "test1"}`}</title>
-      <meta name="description" content={`${false ? "Admin Content" : "test2"}`} />
-      <meta property="og:title" content="새로운선택"/>
-      <meta property="og:description" content="새로운선택"/>
+      <title>{repo.title}</title>
+      <meta name="description" content={repo.abstracts} />
+      <meta property="og:title" content={repo.title}/>
+      <meta property="og:description" content={repo.abstracts}/>
       <meta property="og:image" content="https://newparty.kr/icons/newparty.jpg"/>
       <meta property="og:type" content="website"/>
     </Head>
-    bot test
-    <div>b: {router.query.md}</div>
+    <div>
+      {repo.title}<br/>
+      {repo.author}<br/>
+      {repo.pub_date}<br/>
+      {repo.abstracts}
+    </div>
   </>;
 };
 
 export default Detail;
 
-export const getServerSideProps = async () => {
-  return { props: {} };
+export async function getServerSideProps(context: any) {
+  // console.log(context.query.md)
+  const articles = await fetch(`https://lcw99.github.io/newparty-cms/article/articles.json`).then((res) => res.json())
+  const article = context.query.md
+  var repo = {title: "새로운선택", author: "새로운선택", pub_date: "2000-1-1 11:11", article: "새로운선택", abstracts: "새로운선택"}
+  for (let a of articles) {
+    if (a.article == article.replace("article-", "")) {
+      // console.log('found')
+      repo = a
+    }
+  }
+  // console.log(repo)
+  return { props: repo };
 }
