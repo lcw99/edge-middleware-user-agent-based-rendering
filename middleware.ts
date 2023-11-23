71ba1bccbe4c8fe5e617ce713ff0add60c429195
chang <changlee99@gmail.com>
2023-11-23 11:04:54 +0900

    aa

--- middleware.ts
+++ middleware.ts
@@ -7,14 +7,14 @@ export const config = {
 
 export function middleware(req: NextRequest) {
   // Parse user agent
-  const { device, browser } = userAgent(req)
-  console.log(browser)
+  const ua = userAgent(req)
+  console.log(ua)
   // Check the viewport
   // const viewport = device.type === 'mobile' ? 'mobile' : 'desktop'
-  const viewport = browser.name?.toLowerCase().includes("bot") ? 'mobile' : 'desktop'
+  const viewport = ua.browser.name?.toLowerCase().includes("bot") ? 'mobile' : 'desktop'
   // Update the expected url
   req.nextUrl.pathname = `_viewport/${viewport}`
-  req.nextUrl.searchParams.set("b", `${browser.name}`)
+  req.nextUrl.searchParams.set("b", `${ua.ua}`)
   // console.log(req.nextUrl)
   // Return rewrited response
   return NextResponse.rewrite(req.nextUrl)
